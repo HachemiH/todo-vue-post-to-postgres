cd059108090ad3dc4e4d73c88765257c7457d8d0
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-11-18 15:15:57 +0100

    create controllers & routing for todos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+const router = express.Router();
+const todosController = require("../server/controllers").todos;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/api", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to the Todos API!"
+  })
+);
+
+router.post("/api/todos", todosController.create);
 
 module.exports = router;
