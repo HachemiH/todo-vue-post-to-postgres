0514337598e729ff0d19368807948df0ac1bf958
Hachemi <hachemi@protonmail.com>
2019-11-19 22:30:27 +0100

    Add front vue post todos on db

---
+++ frontend/pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Vue Axios Post</div>
+
+          <div class="card-body">
+            <form @submit="formSubmit">
+              <strong>Name:</strong>
+
+              <input type="text" class="form-control" v-model="name" />
+              <button class="btn btn-success">Submit</button>
+            </form>
+
+            <strong>Output:</strong>
+            <pre>
+              {{output}}
+            </pre>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+     
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+
+  data() {
+    return {
+      name: "",
+      output: ""
+    };
+  },
+
+  methods: {
+    formSubmit(e) {
+      e.preventDefault();
+      let currentObj = this;
+
+      this.axios
+        .post("http://localhost:4000/api/todos", {
+          title: this.name
+        })
+
+        .then(function(response) {
+          currentObj.output = response.data;
+        })
+
+        .catch(function(error) {
+          currentObj.output = error;
+        });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Todo :</h1>
+    <form @submit="addTodo">
+      <input type="text" class="form-control" v-model="input" />
+      <button class="btn btn-success">Submit</button>
+    </form>
+    <div>
+      <h2>Saved :</h2>
+      <pre>
+       {{ saved }}
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {},
+
+  data() {
+    return {
+      input: "",
+      saved: ""
+    };
+  },
+
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+
+      this.axios
+        .post("http://localhost:4000/api/todos", {
+          title: this.input
+        })
+        .then(response => {
+          this.saved = response.data;
+        })
+        .catch(error => {
+          this.saved = error;
+        });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
