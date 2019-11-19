df0a6ae4063e32d137f8df904cfef7cceaf2ef52
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-11-19 11:52:24 +0100

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+const cors = require("cors");
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
